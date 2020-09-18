fefa1b52b0287b048d034df36dd56f69a54d62b5
vamsimudadla <mudadlavamsi@gmail.com>
2020-09-18 10:20:57 +0530

    implemented errors store

--- src/stores/ErrorsStore/index.js
+++ src/stores/ErrorsStore/index.js
@@ -1,10 +1,39 @@
-import { observable } from "mobx";
+import { observable, action } from "mobx";
+
+import { apiStatus } from "../../constants/apiConstants";
 
 class ErrorsStore {
   @observable errorsList;
+  @observable getErrorsListAPIStaus;
 
   constructor() {
     this.errorsList = [];
+    this.getErrorsListAPIStaus = apiStatus.apiInitial;
+  }
+
+  @action.bound
+  setGetErrorsListAPIStatus(apiStatus) {
+    this.setGetErrorsListAPIStatus = apiStatus;
+  }
+
+  @action.bound
+  setGetErrorsListAPIResponse(response) {
+    this.errorsList = response;
+  }
+
+  @action.bound
+  getErrorsList() {
+    const { apiLoading, apiSuccess, apiFailed } = apiStatus;
+    this.setGetErrorsListAPIStatus(apiLoading);
+    fetch("https://api-flash.supplycompass.com/ukyzGAndExrbNdU/json")
+      .then((response) => response.json())
+      .then((responseJSON) => {
+        this.setGetErrorsListAPIResponse(responseJSON);
+        this.setGetErrorsListAPIStatus(apiSuccess);
+      })
+      .catch((error) => {
+        this.setGetErrorsListAPIStatus(apiFailed);
+      });
   }
 }
 
